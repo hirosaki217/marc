bf8c09a8f51d1ce1ad70e88dbedb077cd77201cf
hieu <hirosaki217@gmail.com>
2025-05-18 12:39:26 +0700

    setup prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-
 export default function Home() {
-  return (
-    <div className="">
-      hello next 15
-    </div>
-  );
+  return <div className=""></div>
 }
---
+++ src/components/HomeCarousel.tsx
@@ -0,0 +1,5 @@
+function HomeCarousel() {
+  return <div></div>
+}
+
+export default HomeCarousel
